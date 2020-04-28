95d26da0bc32dcc9318e6ac335690ee491bbc97b
Jens Duray <bobaflex.hsv@gmail.com>
2020-04-28 12:18:26 +0200

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -10,23 +11,44 @@ const pool = new Pool({
  port: 5432,
 });
 
-// ***** STEP 1 *****
+app.use(bodyParser.json())
+
+// ***** STEP 1: GET  /  : get all the users *****
 app.get("/", (req, res) => {
     pool 
       .query('SELECT * FROM users;')
       .then(data => res.json(data))
       .catch(e => res.sendStatus(404)); 
-  });
+});
   
-  // ***** STEP 2 *****
-  app.get("/:id", (req, res) => {
-    const { id } = req.params; 
-    pool
+// ***** STEP 2: GET  /:id :  get one user (with the id)  *****
+app.get("/:id", (req, res) => {
+  const { id } = req.params; 
+  pool
       .query('SELECT * FROM users WHERE id=$1;', [id])
       .then(data => res.json(data)) 
       .catch(e => res.sendStatus(404)); 
+});
+   
+// ***** STEP 3: POST / -> create a new user  *****
+
+/* TEST DATA
+{
+            "first_name": "Donald",
+            "last_name": "Duck",
+            "age": 24
+        }
+*/
+
+app.post("/", (req, res) => {
+   const {first_name, last_name, age} = req.body;
+    pool
+      .query('INSERT INTO users(first_name, last_name, age) values($1,$2,$3);', [first_name, last_name, age])
+      .then(data => res.status(201).json(data))
+      .catch(e => res.sendStatus(404)); 
    });
    
+
 
 app.listen('3000', () => console.log('Connected to server localhost:3000...'));
 
