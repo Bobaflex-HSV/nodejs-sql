644df7aeef587aaeb94611ce702818041ceab55a
Jens Duray <bobaflex.hsv@gmail.com>
2020-04-28 13:34:14 +0200

    add get/:id route to orders

--- orders.js
+++ orders.js
@@ -21,3 +21,19 @@ app.get("/", (req, res) => {
 });
 
 app.listen('3000', () => console.log('Connected to server localhost:3000...'));
+
+// ***** STEP 7: GET  /:id :  To get one order (with the id)  *****
+app.get("/:id", (req, res) => {
+    const { id } = req.params; 
+    pool
+        .query('SELECT * FROM orders WHERE id=$1;', [id])
+        .then(data => res.json(data)) 
+        .catch(e => res.sendStatus(404)); 
+  });
+
+
+
+
+// ***** STEP 8: POST / -> To create a new order *****
+// ***** STEP 9: PUT /:id  :  To edit one order (with the id) *****
+// ***** STEP 10: DELETE  /:id : To delete one order (with the id)  *****
